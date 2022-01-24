30826f83ddf7726ea458c0662c32e653dfaf35fc
Akash Patil <akash.patil@rsllearning.com>
2022-01-24 18:58:54 +0530

    assignment completed

--- app/src/main/java/com/rajasoftwarelabs/intentsassignment/MainActivity.java
+++ app/src/main/java/com/rajasoftwarelabs/intentsassignment/MainActivity.java
@@ -1,8 +1,12 @@
 package com.rajasoftwarelabs.intentsassignment;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -13,6 +17,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,8 +29,12 @@ public class MainActivity extends AppCompatActivity {
     private static final int MAX_COFFEES = 20; // Varies from developer to developer :)
     // endregion
 
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
     private int numCoffees = MIN_COFFEES;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private void setupCameraIntent() {
         final Button takePhotoButton = findViewById(R.id.take_photo_button);
 
@@ -109,19 +119,68 @@ public class MainActivity extends AppCompatActivity {
 
     private void fireExplicitIntent(@NonNull String name) {
         // TODO (1): Create and fire an explicit intent to open NameActivity
+        Intent nameIntent = new Intent(MainActivity.this,NameActivity.class);
+        nameIntent.putExtra("name",name);
+        startActivity(nameIntent);
     }
 
     private void fireEmailIntent(@NonNull String name) {
         // Populate the subject and body template strings with the name and number of coffees.
-        String subject = getString(R.string.email_subject, name);
-        String body = getResources().getQuantityString(R.plurals.email_body, numCoffees, numCoffees, name);
+        String subject = "Coffee order for "+name;
+        String body = "Coffee order for " + name + " of "+numCoffees + " coffees ";
 
         // TODO (4): Create and fire an implicit intent to open the email app. The email address to send to is the
         //           EMAIL_ADDRESS constant.
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, EMAIL_ADDRESS);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+
+        startActivity(Intent.createChooser(emailIntent, "Send Email"));
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private void fireCameraIntent() {
         // TODO (5): Fire an implicit intent to open the camera and get an image as a result from it.
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+        }
+        else
+        {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+            else
+            {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            setImage(photo);
+        }
     }
 
     // TODO (6): Capture the response from the camera. You can use the setImage method to show the returned thumbnail.
--- app/src/main/java/com/rajasoftwarelabs/intentsassignment/NameActivity.java
+++ app/src/main/java/com/rajasoftwarelabs/intentsassignment/NameActivity.java
@@ -1,18 +1,39 @@
 package com.rajasoftwarelabs.intentsassignment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 // TODO (3): Add back navigation to the Toolbar. You might find some hints in the reading material provided in the
 //           training document.
 public class NameActivity extends AppCompatActivity {
 
+    TextView message;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -22,6 +43,8 @@ public class NameActivity extends AppCompatActivity {
         // TODO (2): Get the name in the intent sent from MainActivity
         String name = null;
         TextView nameTextView = findViewById(R.id.name_text_view);
-        nameTextView.setText(getString(R.string.name_activity_text, name));
+        Bundle extras = getIntent().getExtras();
+        name = extras.getString("name");
+        nameTextView.setText("Hello "+name);
     }
 }
